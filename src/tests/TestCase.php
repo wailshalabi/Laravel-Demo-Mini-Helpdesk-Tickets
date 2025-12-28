be4adf5a398913d061bbb6fe2e863a285cb113f9
Wail Alshalabi <wail@wailshalabi.com>
2025-12-28 10:56:24 +0100

    Fix phpunit test enviroment and move it to memory based

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -13,5 +13,10 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         $this->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class);
+
+        if (config('database.default') === 'mysql' && config('database.connections.mysql.database') === 'laravel') {
+            throw new \RuntimeException('For security reasons, refusing to run tests on the dev database (mysql:laravel).');
+        }
+
     }
 }
--- src/tests/Unit/UserTest.php
+++ src/tests/Unit/UserTest.php
@@ -12,6 +12,13 @@ class UserTest extends TestCase
 
     public function test_is_admin_and_agent_helpers(): void
     {
+
+        dump([
+            'env' => app()->environment(),
+            'db_default' => config('database.default'),
+            'db_name' => config('database.connections.' . config('database.default') . '.database'),
+        ]);
+
         $admin = User::factory()->create(['role' => 'admin']);
         $agent = User::factory()->create(['role' => 'agent']);
         $user = User::factory()->create(['role' => 'user']);
